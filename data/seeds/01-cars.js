c1306fc6105c67af340a9f31ffe91656c42d729b
CamMeiners <cammeiners@gmail.com>
2021-11-02 21:42:01 -0600

    out of time, will have to finish post tomorrow

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,42 @@
 // STRETCH
+const cars = [
+  {
+    vin: '1GNDM19Z5NB205914',
+    make: 'Chevy',
+    model: 'Astro',
+    mileage: 180810,
+    title: 'clean',
+    transmission: 'automatic',
+  },
+  {
+    vin: '1GNDM19Z5NB205915',
+    make: 'Chevy',
+    model: 'Astro',
+    mileage: 180810,
+    title: 'clean',
+    transmission: 'automatic',
+  },
+  {
+    vin: '1GNDM19Z5NB205916',
+    make: 'Chevy',
+    model: 'Astro',
+    mileage: 180810,
+    title: 'clean',
+    transmission: 'automatic',
+  },
+  {
+    vin: '1GNDM19Z5NB205917',
+    make: 'Chevy',
+    model: 'Astro',
+    mileage: 180810,
+    title: 'clean',
+    transmission: 'automatic',
+  },
+];
+exports.seed = function (knex) {
+  return knex('cars')
+    .truncate()
+    .then(() => {
+      return knex('cars').insert(cars);
+    });
+};
